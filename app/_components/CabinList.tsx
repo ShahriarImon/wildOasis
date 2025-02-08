cf01964e650720f581aa2e528d8425bc19dc3f63
ShahriarImon <imon.shahriar012@gmail.com>
2025-02-08 10:45:31 +0600

    checking with force-dynamic

--- app/_components/CabinList.tsx
+++ app/_components/CabinList.tsx
@@ -9,6 +9,7 @@ export interface singleCabin {
   discount: number;
   image: string;
 }
+export const dynamic = "force-dynamic";
 
 const CabinList = async () => {
   const cabins: [] | singleCabin[] = await getCabins();
