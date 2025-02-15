06ea0541ef6edc32bb81a0e5c63c5e9e09547960
ShahriarImon <imon.shahriar012@gmail.com>
2025-02-15 14:19:56 +0600

    All test done.. Great performance

--- app/cabins/[cabinID]/page.tsx
+++ app/cabins/[cabinID]/page.tsx
@@ -1,10 +1,7 @@
 import React from "react";
-// import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
-// import {
-//   getCabin,
-//   // getCabins
-// } from "@/app/_lib/data-service";
-// import Image from "next/image";
+import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
+import Image from "next/image";
 
 // PLACEHOLDER DATA
 interface Cabin {
@@ -28,34 +25,34 @@ interface CabinProps {
 //   return data?.map((item) => ({ cabinID: String(item.id) }));
 // }
 
-// export async function generateMetadata({ params }: CabinProps) {
-//   const { name, image, description } = await getCabin<Cabin>(params?.cabinID);
+export async function generateMetadata({ params }: CabinProps) {
+  const { name, image, description } = await getCabin<Cabin>(params?.cabinID);
 
-//   return {
-//     title: `Cabin-${name}`,
-//     description: description,
-//     image: image,
-//   };
-// }
+  return {
+    title: `Cabin-${name}`,
+    description: description,
+    image: image,
+  };
+}
 
 const Cabin = async ({ params }: CabinProps) => {
-  const response = await fetch(
-    "https://news-letter-backend-4sxo.onrender.com/api/article/" +
-      params?.cabinID,
-    {
-      cache: "no-cache",
-    }
-  );
-  const data = await response.json();
-  console.log("xcv:", data?.data?.title);
-  // const { name, maxCapacity, image, description } = await getCabin<Cabin>(
-  //   params?.cabinID
+  // const response = await fetch(
+  //   "https://news-letter-backend-4sxo.onrender.com/api/article/" +
+  //     params?.cabinID,
+  //   {
+  //     cache: "no-cache",
+  //   }
   // );
+  // const data = await response.json();
+  // console.log("xcv:", data?.data?.title);
+  const { name, maxCapacity, image, description } = await getCabin<Cabin>(
+    params?.cabinID
+  );
   // console.log("description121212:", description);
   return (
     <div className="max-w-6xl mx-auto mt-8">
-      {data?.data?.title}
-      {/* <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
+      {/* {data?.data?.title} */}
+      <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
         <div className="relative scale-[1.15] -translate-x-3">
           <Image
             fill
@@ -101,7 +98,7 @@ const Cabin = async ({ params }: CabinProps) => {
         <h2 className="text-5xl font-semibold text-center">
           Reserve today. Pay on arrival.
         </h2>
-      </div> */}
+      </div>
     </div>
   );
 };
